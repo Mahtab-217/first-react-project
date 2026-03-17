bd36dd20fc8bab0b541a50a68ace6fbb257d1486
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-17 09:00:24 +0430

    new components

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    let name= "Mahtab";
+     return (
+        <div>
+            <h1>Hi I am {name}</h1>
+            <h1>Hi from Header Section</h1>
+        </div>
+     )
+}
---
+++ src/Hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero(){
+    function greeting(){
+        console.log("Salam")
+    }
+    greeting();
+    return [
+        <div>
+            <h1>
+                Hi From Hero Section
+            </h1>
+        </div>
+    ]
+}
