e96670e8e9609da88f5016e10d61cce02a4ab3d0
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-19 09:44:53 +0430

    new style for teacher list

--- src/Teachers.jsx
+++ src/Teachers.jsx
@@ -1,7 +1,21 @@
-export default function Teachers(){
+import "./style.css";
+export default function Teachers(props){
     return (
         <div>
             <h1>List of The Teachers</h1>
+            <div >
+              {props.tList.length>0 &&(
+                   <div className="parent">
+                    {props.tList.map((t)=>(
+                        <div key={t.id} className="box">
+                            <h1>{t.name}</h1>
+                            <h2>{t.lastName}</h2>
+                            <h3>{t.age}</h3>
+                        </div>
+                    ))}
+                   </div>
+              )}
+            </div>
         </div>
     )
 }
